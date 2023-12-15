48020d0d7c64aa80924fa2019e6607a8e04b4962
ariadnamarcos <145438133+ariadnamarcos@users.noreply.github.com>
2023-12-15 23:31:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,59 +3,100 @@ import threading
 import gi
 import json
 #from nfc import Rfid
-import urllib.request
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk, GLib
-from TimerHandler import TimerHandler
+import http.client
+import time
+from timeit  import Timer
+from timer_handler import newTimer
+
+class Temporizador:
+
+    def __init__(self):
+        self.x = 10 #nombre de segons
+        th = threading.Thread(target = self.count)
+        th.start()
+    
+    def count(self):
+        x=10
+        while(True):
+            x=x-1
+            time.sleep(1)
+            if x == 0:
+                self.logout()
+
 
 class CourseManager: 
 
     def __init__(self):
         self.user = None
         self.uid = None
-        self.user_lock = threading.Lock()
-        self.timer_handler = TimerHandler(10, self.logout)
+        self.table = None
+        self.conn = http.client.HTTPConnection("localhost", 8080)
         self.login()
-        while(self.user is not None):
-            self.consultarServer()
+        self.flag = False
+        
+
+    def count(self):
+        x = 5
+        while(True):
+            x=x-1
+            time.sleep(1)
+            if x == 0 and self.flag==True:
+                    self.logout()
+            else:
+                self.flag=False
+            
+    def get(self, url):
+        try:
+            self.conn.request("GET", url)
+            response = self.conn.getresponse()
+            if response.status == 200:
+            # Read and print the content of the response
+                json_arr = response.read().decode('utf-8')  
+                return json.loads(json_arr) #retornem l'objecte json
+        finally:
+            self.conn.close()
+
 
     def login(self):
-        print ("Entra el teu uid: ")    #7399A831, 1099031C
+        print ("Entra el teu uid: ")    #D1FDE202, 938B506
         self.uid = input()
-        url = "http://localhost:8080/CriticalDesignPBE/back/index.php/students?uid={}".format(self.uid)
-        request = urllib.request.Request(url)
-        try:
-            with urllib.request.urlopen(request) as response:
-            # Read the response data
-                json_array = response.read().decode('utf-8')
-                data = json.loads(json_array)
-                with self.user_lock:
-                    self.user = data[0]['userName']
-            print('sessió iniciada: ' + self.user)
-        except urllib.error.URLError as e:
-            print(f"Error al realizar la solicitud: {e}")
-        timer_thread = threading.Thread(target=self.timer_handler.reset_timer)
-        timer_thread.start()
-        
+        data = self.get("/CriticalDesignPBE/back/index.php/students?uid={}".format(self.uid))
+        if data:
+            self.user = data[0]['userName']
+            print(data)
+            print(self.user)
+            self.consultarServer()
+
     def logout(self):
-        with self.user_lock:
-            self.user = None
-            self.uid = None
+        self.user = None
+        self.uid = None
+        if self.conn:
+            self.conn.close()
         self.login()
 
     def consultarServer(self):
-        print("Introduzca lo que quiera ver: ")
-        self.table = input()
-        if self.table:
-            self.timer_handler.reset_timer()
-            if self.table == 'logout':
-                self.logout()
-            elif self.uid is not None:
-                url = "http://localhost:8080/CriticalDesignPBE/back/index.php/{}?uid={}".format(self.table, self.uid)
-                request = urllib.request.Request(url)
-                with urllib.request.urlopen(request) as response:
-                    json_array = response.read().decode('utf-8')
-                    print(json_array);
+        
+        while(True):
+            
+            print("Introdueix el que vols veure: ")
+            th = threading.Thread(target = self.count())
+            th.start()
+            self.table = input()
+            
+            if self.table:
+                if self.table == 'logout':
+                    self.logout()
+                else:
+                    self.aux = self.table.split("?") #mirem primera part de la query per saber si és marks i enviar uid
+                    if (self.aux[0]=="marks" and self.uid):
+                        if(len(self.aux)==1):
+                            self.table = self.table + "?uid=" + self.uid
+                    data = self.get("/CriticalDesignPBE/back/index.php/{}".format(self.table))
+                    print(data)
+                self.flag = True
+            
+course = CourseManager()
 
-course = CourseManager();
 Gtk.main()
